99e72d372ea95766196737ed56f1a843af0d54cb
krystiano13 <krystiano13@interklasa.pl>
2020-10-02 21:39:56 +0200

    Updates

---
+++ precache-manifest.a269170250a008f463ecb670d8a90aa0.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98660c6ac36bc8b4e316c727529346e2",
+    "url": "/scenic/index.html"
+  },
+  {
+    "revision": "39a91615fbbdeb29838e",
+    "url": "/scenic/static/css/main.9dd0fc06.chunk.css"
+  },
+  {
+    "revision": "2432aef4b18caa88fa89",
+    "url": "/scenic/static/js/2.817aba02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/scenic/static/js/2.817aba02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39a91615fbbdeb29838e",
+    "url": "/scenic/static/js/main.4d6b08e4.chunk.js"
+  },
+  {
+    "revision": "5e4b683ed28912e972c3",
+    "url": "/scenic/static/js/runtime-main.1a2a64dc.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/scenic/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "7a713e6a8958b43395059ec287ce094c",
+    "url": "/scenic/static/media/project-image1.7a713e6a.jpg"
+  },
+  {
+    "revision": "5cec96584f5076706048d982197297f5",
+    "url": "/scenic/static/media/project-image2.5cec9658.jpg"
+  },
+  {
+    "revision": "3fcb156299e6e72fd64766b82698bba3",
+    "url": "/scenic/static/media/project-image3.3fcb1562.jpg"
+  },
+  {
+    "revision": "214d653cede5ba97ce7e0323cfd17c38",
+    "url": "/scenic/static/media/project-image4.214d653c.jpg"
+  },
+  {
+    "revision": "43fcfed32dbfeef17fc70269d776bae8",
+    "url": "/scenic/static/media/team-image1.43fcfed3.jpg"
+  },
+  {
+    "revision": "40813e8fe9f5863e648f857e9fa09235",
+    "url": "/scenic/static/media/team-image2.40813e8f.jpg"
+  },
+  {
+    "revision": "e687042a7ab9a7e6c4b31690737a3107",
+    "url": "/scenic/static/media/team-image3.e687042a.jpg"
+  },
+  {
+    "revision": "644ec0e1824417cd9c5d86b980ddef49",
+    "url": "/scenic/static/media/team-image4.644ec0e1.jpg"
+  },
+  {
+    "revision": "a95af86a15596ab022540d1e9adcc776",
+    "url": "/scenic/static/media/video.a95af86a.mp4"
+  }
+]);
